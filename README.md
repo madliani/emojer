0b20baba774a2b7ee8d0e45d7ba063e954124ad5
crystallographer <r0475582@proton.me>
2023-04-06 22:51:08 +0400

    feat: readme and emojer class added

---
+++ README.md
@@ -0,0 +1,40 @@
+# Emojer
+
+<div align="center">
+    <img src="./assets/images/emoji.png" alt="emoji" />
+
+    The Emoji Compiler
+
+</div>
+
+![Built with Ruby](https://img.shields.io/badge/Ruby-CC342D?style=for-the-badge&logo=ruby&logoColor=white)
+
+This is a compiler for the Brainf\*ck programming language dialect with an emoji-like command system.
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `rake build`
+
+Runs the app.
+
+### `rake format`
+
+Runs the code formatter.
+
+### `rake lint`
+
+Runs the code linters.
+
+### `rake test`
+
+Launches the test runner.
+
+## Authors
+
+Rakhman Abdurakhmanov - [Portfolio](https://crystallographer.github.io) |
+[GitHub](https://github.com/crystallographer).
+
+Based on articles [Let’s Write a Brainfuck Compiler (in JavaScript)](https://itnext.io/lets-write-a-brainfuck-compiler-aaa7cfd449b0) by
+[Michele Riva](https://micheleriva.medium.com), [Интерпретатор brainfuck](https://pythonworld.ru/primery-programm/interpretator-brainfuck.html) and [Little Lisp interpreter](https://maryrosecook.com/blog/post/little-lisp-interpreter) by [Mary Rose Cook](https://maryrosecook.com).
---
+++ src/emojer.rb
@@ -0,0 +1,79 @@
+# Emojer's compiler
+class Emojer
+  COMMANDS = ["+", ",", "-", ".", "<", ">", "[", "]"].freeze
+
+  def initialize(program = [])
+    @parentheses = {}
+    @program = program
+  end
+
+  def tokenize
+    @program = @program.chomp.strip.chars.map.with_index do |instruction, program_ptr|
+      @program[program_ptr + 1] if instruction == ":" && COMMANDS.include?(@program[program_ptr + 1])
+    end
+
+    @program = @program.freeze
+  end
+
+  def parenthesize
+    opened = []
+
+    @program.each.with_index do |instruction, program_ptr|
+      case instruction
+      when COMMANDS[6]
+        opened << program_ptr
+      when COMMANDS[7]
+        @parentheses[program_ptr] = opened.last
+        @parentheses[opened.pop] = program_ptr
+      end
+    end
+
+    @parentheses = @parentheses.freeze
+  end
+
+  def parse
+    tokenize
+    parenthesize
+  end
+
+  def evaluate
+    code = []
+    memory = [0]
+    memory_ptr = 0
+    program_ptr = 0
+
+    while program_ptr < @program.length
+      instruction = @program[program_ptr]
+
+      case instruction
+      when COMMANDS[0]
+        memory[memory_ptr] += 1
+      when COMMANDS[1]
+        memory[memory_ptr] << gets.chop.ord
+      when COMMANDS[2]
+        memory[memory_ptr] -= 1
+      when COMMANDS[3]
+        code << memory[memory_ptr].chr
+      when COMMANDS[4]
+        memory_ptr -= 1
+      when COMMANDS[5]
+        memory_ptr += 1
+        memory[memory_ptr] ||= 0
+        memory[memory_ptr] ||= 0
+      when COMMANDS[6]
+        program_ptr = @parentheses[program_ptr] if memory[memory_ptr].zero?
+      when COMMANDS[7]
+        program_ptr = @parentheses[program_ptr] if memory[memory_ptr].nonzero?
+      end
+
+      program_ptr += 1
+    end
+
+    code.join("")
+  end
+
+  def compile
+    parse
+    evaluate
+  end
+end
